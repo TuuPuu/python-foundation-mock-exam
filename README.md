9c7634a7cbe104de41133b77f79b72cfa3b5a11c
TuPu <inspirontab@hotmail.com>
2024-06-17 19:37:45 +0100

    Part 3 Coding Challenge

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # python-foundation-mock-exam
- Summer '24 Foundation Track 1 (Python & SQL) MOCK Exam
+
+Summer '24 Foundation Track 1 (Python & SQL) MOCK Exam
+Elora Ainoa (Aboua)
+
+Answers for the main portion of the mock exam are found within the python file: main.py
+
+Answers for the theory questions are contained below:
+
+
+# THEORY QUESTIONS
+
+1.1 
--- main.py
+++ main.py
@@ -58,5 +58,55 @@ Finally we could add in some code in our parameters to handle potential null val
 
 ________________________________________________________
 
+PART 3 CODING CHALLENGE
+
+(find code below)
 
 '''
+
+
+def calculate_change(cost):
+  
+    coins = {
+        2.0: 2,
+        1.0: 6,
+        0.5: 10,
+        0.2: 4,
+        0.1: 6,
+        0.05: 7,
+        0.02: 4,
+        0.01: 6
+    }
+
+    change_dict = {
+        2.0: 0,
+        1.0: 0,
+        0.5: 0,
+        0.2: 0,
+        0.1: 0,
+        0.05: 0,
+        0.02: 0,
+        0.01: 0
+    }
+    
+
+    total_coins = sum(coins[value] for value in coins)
+
+    change_needed = total_coins - cost
+
+    if change_needed < 0:
+        return "Can't afford this with the available petty change"
+
+    for value in sorted(coins.keys(), reverse=True):
+        if change_needed <= 0:
+            break
+        num_coins = min(change_needed // value, coins[value])
+        change_dict[value] = num_coins
+        change_needed -= num_coins * value
+    
+    return change_dict
+
+
+print(calculate_change(12.50)) 
+print(calculate_change(16.89)) 
+print(calculate_change(16.90))

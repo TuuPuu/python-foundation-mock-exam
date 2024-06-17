9febba708e326f4952932a8196fba5b6ce29b2f4
TuPu <inspirontab@hotmail.com>
2024-06-17 19:15:59 +0100

    Part 2 Evaluation- 01

--- main.py
+++ main.py
@@ -22,9 +22,31 @@ QUESTION 09: A
 
 QUESTION 10: B
 
+________________________________________________________
+
+PART 2 EVALUATION
+
+
+1)
+
+Firstly, here is a syntax error after the department id declaration. A comma is missing. It should instead be:
+
+SELECT
+    department_id,
+    COUNT(*) AS total_admissions
+    
+    
+Secondly, the columns will be mismatched as the group by is using the declaration department_name. Instead it should use department_id, like this:
+
+GROUP BY
+    department_id;
 
 
 
-____________________________
+
+
+
+
+
 
 '''

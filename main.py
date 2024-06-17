065867c64d9865bcbf31f45aa3adeaf15c89fc87
TuPu <inspirontab@hotmail.com>
2024-06-17 19:02:48 +0100

    Multiple Choice Questions

---
+++ main.py
@@ -0,0 +1,30 @@
+'''
+
+PART 1 MULTIPLE CHOICE
+
+QUESTION 01: A
+
+QUESTION 02: C
+
+QUESTION 03: A
+
+QUESTION 04: D
+
+QUESTION 05: B
+
+QUESTION 06: C
+
+QUESTION 07: D
+
+QUESTION 08: A
+
+QUESTION 09: A
+
+QUESTION 10: B
+
+
+
+
+____________________________
+
+'''
